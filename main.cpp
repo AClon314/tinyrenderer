8de85096449290fa5fe91b79bbfca8792d78553b
ssloy <spam@skisa.org>
2016-02-01 14:32:54 +0100

    Lesson3: zbuffer, need light

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+#include <vector>
+
 #include "geometry.h"
 #include "model.h"
 #include "tgaimage.h"
@@ -41,19 +46,16 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 }
 
 // 枚举一个包围盒中所有的像素，找到在三角形内的像素，计算该像素的重心坐标。如果有分量是负数，那这个像素就不在三角形内。lesson 2
-Vec3f centroid(Vec3f *pts, Vec3f P) {
+Vec3f barycentric(Vec3f *pts, Vec3f P) {
     // (AB,AC,PA)的x与y分量做叉积，得到垂直于面的向量
-    Vec3f u = Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]) ^ Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]);
-    /* `pts` and `P` has integer value as coordinates
-        so `abs(u[2])` < 1 means `u[2]` is 0, that means
-        triangle is degenerate, in this case return something with negative coordinates
-        u是三角形ABC的法向量
-        u.x=AC,PA面积
-        u.y=AB,PA面积
-        u[2]=u.z是行列式，即AB,AC围起来的平行四边形的面积
-    */
-    if (std::abs(u[2]) < 1) return Vec3f(-1, 1, 1);               // 三角形太小，返回一个负数
-    return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);  // (a,b,c可互换，与顺序无关) 返回重心坐标的权重：三角形内任意一点，能表示成三个顶点的线性组合
+    Vec3f u = cross(Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]),
+                    Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]));
+    // u是三角形ABC的法向量
+    // u.x=AC,PA面积，u.y=AB,PA面积
+    // u.z是行列式，即AB,AC围起来的平行四边形的面积
+    if (std::abs(u[2]) > 1e-2)                                        // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
+        return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);  // (a,b,c可互换，与顺序无关) 返回重心坐标的权重：三角形内任意一点，能表示成三个顶点的线性组合
+    return Vec3f(-1, 1, 1);                                           // 三角形太小，返回一个负数 in this case generate negative coordinates, it will be thrown away by the rasterizator
 }
 
 // 画三角形+zbuffer，lesson 2~3
@@ -71,50 +73,55 @@ void triangle(Vec3f *t, float *zbuffer, TGAImage &image, TGAColor color) {
     Vec3f P;
     for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
         for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
-            // 逐点绘制
-            Vec3f bc_screen = centroid(t, P);  // barycentric coordinate
+            Vec3f bc_screen = barycentric(t, P);
             if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
             P.z = 0;
-            for (int i = 0; i < 3; i++) P.z += t[i][2] * bc_screen[i];  // 点z * 对应点的重心坐标的权重
-            if (zbuffer[int(P.x + P.y * width)] < P.z) {
-                // z越小越靠近camera
-                zbuffer[int(P.x + P.y * width)] = P.z;
+            for (int i = 0; i < 3; i++) P.z += t[i][2] * bc_screen[i];
+            int idx = int(P.x + P.y * width);
+            if (zbuffer[idx] < P.z) {
+                zbuffer[idx] = P.z;
                 image.set(P.x, P.y, color);
             }
         }
     }
 }
 
+Vec3f world2screen(Vec3f v) {
+    return Vec3f(int((v.x + 1.) * width / 2. + .5), int((v.y + 1.) * height / 2. + .5), v.z);
+}
+
 int main(int argc, char **argv) {
     if (2 == argc) {
         model = new Model(argv[1]);
     } else {
-        model = new Model("../head.obj");
+        model = new Model("../african_head/african_head.obj");
     }
 
     float *zbuffer = new float[width * height];
     for (int i = width * height; i--; zbuffer[i] = -FLOAT_MAX);
 
     TGAImage image(width, height, TGAImage::RGB);
-    Vec3f light_dir(0, 0, -1);
+    // Vec3f light_dir(0, 0, -1);
     for (int i = 0; i < model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3];
-        Vec3f world_coords[3];  // face的三个顶点
-        for (int j = 0; j < 3; j++) {
-            Vec3f v = model->vert(face[j]);
-            screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);  // -1~1 mapping to 0~width/height
-            world_coords[j] = v;
-        }
-        Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);  // 法向量
-        n.normalize();
-        float intensity = n * light_dir;  // 光照强度
-        if (intensity > 0) {
-            triangle(world_coords, zbuffer, image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
-        }
-        // ​ 注意，口腔的内部被绘制到了嘴唇的顶部。只是因为我们对不可见三角形的拙劣裁剪，只能在凸多边形上才能正常工作。我们会在写出 z-buffer 后处理掉这个阴影。
-    }
+        // Vec2i screen_coords[3];
+        // Vec3f world_coords[3];  // face的三个顶点
+        Vec3f pts[3];
+        for (int i = 0; i < 3; i++) pts[i] = world2screen(model->vert(face[i]));
+        triangle(pts, zbuffer, image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
 
+        // for (int j = 0; j < 3; j++) {
+        //     Vec3f v = model->vert(face[j]);
+        //     screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);  // -1~1 mapping to 0~width/height
+        //     world_coords[j] = v;
+        // }
+        // Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);  // 法向量
+        // n.normalize();
+        // float intensity = n * light_dir;  // 光照强度
+        // if (intensity > 0) {
+        //     triangle(world_coords, zbuffer, image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+        // }
+    }
     image.flip_vertically();
     image.write_tga_file("output.tga");
     delete model;
