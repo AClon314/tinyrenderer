b0a8af20c696b26dd1e69b74df5f7df447b3c2ae
AClon314 <30747832+AClon314@users.noreply.github.com>
2025-01-02 21:37:03 +0800

    Lesson 2: 随机色彩head.obj

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "tgaimage.h"
 #include "geometry.h"
+#include "model.h"
 /// gprof O0优化，代码质量与执行时间无明显关系
 /// 优化，就是将易理解的短代码，转为难理解的、或更长或更短代码。所以不要太早优化。
 
@@ -9,39 +10,29 @@ const TGAColor red   = TGAColor(255, 0,   0,   255);
 const TGAColor green = TGAColor(0,   255, 0,   255);
 const TGAColor blue  = TGAColor(0,   0,   255, 255);
 const TGAColor rgb[] = {red, green, blue};
+Model *model = NULL;
+const int width  = 800;
+const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     bool steep = false;
-    if (std::abs(x0-x1) < std::abs(y0-y1)){
+    if (std::abs(x0-x1)<std::abs(y0-y1)) {
         std::swap(x0, y0);
         std::swap(x1, y1);
         steep = true;
     }
-    if (x0 > x1) {
+    if (x0>x1) {
         std::swap(x0, x1);
         std::swap(y0, y1);
     }
-    int dx = x1-x0;
-    int dy = y1-y0;
-    // float derror = std::abs(dy)/(float)(dx);     /// x每增加1，y的增量；注意abs(y)，因为swap是保证x是递增的
-    int derror2 = std::abs(dy)*2;                   /// 所有浮点数，都可以转为整数
-    // float error = 0;
-    int error = 0;
-    int y = y0;
-    for (int x=x0; x<=x1; x++) {
-        /// 逐像素绘制
 
-        // float t = (x-x0)/(float)(x1-x0);
-        // int y = y0*(1.-t) + y1*t;
-        if (steep){
+    for (int x=x0; x<=x1; x++) {
+        float t = (x-x0)/(float)(x1-x0);
+        int y = y0*(1.-t) + y1*t;
+        if (steep) {
             image.set(y, x, color);
         } else {
             image.set(x, y, color);
-        }
-        error += derror2;
-        if (error > dx) {
-            y += (y1>y0?1:-1);
-            error -= dx*2;  /// 将所有涉及到的浮点数*dx*2
         }
     }
 }
@@ -82,12 +73,26 @@ void triangle(Vec2i t[], TGAImage &image, TGAColor color) {
 }
 
 int main(int argc, char** argv) {
-    TGAImage image(200, 200, TGAImage::RGB);
-    Vec2i pts[3] = {Vec2i(10, 70),   Vec2i(50, 170),  Vec2i(70, 80)};
-    triangle(pts, image, red);
+    TGAImage image(width, height, TGAImage::RGB);
+    if (2==argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("../head.obj");
+    }
+
+    for (int i=0; i<model->nfaces(); i++) {
+        std::vector<int> face = model->face(i); 
+        Vec2i screen_coords[3]; 
+        for (int j=0; j<3; j++) { 
+            Vec3f world_coords = model->vert(face[j]); 
+            screen_coords[j] = Vec2i((world_coords.x+1.)*width/2., (world_coords.y+1.)*height/2.); 
+        }
+        triangle(screen_coords, image, TGAColor(rand()%255, rand()%255, rand()%255, 255)); 
+}
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
+    delete model;
     return 0;
 }
 
